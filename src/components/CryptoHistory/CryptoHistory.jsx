2b145c1173a8f8d5eec93e761a8d4337cb3760a9
Yaroslav Tkach <yaroslavtkach@outlook.com>
2023-06-24 12:51:16 +0300

    task4 completed

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,30 @@
+import { formatDate } from 'helper/formatDate';
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
 
-export const CryptoHistory = () => {
-  return <div>CryptoHistory</div>;
+export const CryptoHistory = ({ items }) => {
+  return (
+    <BaseTable>
+      <THead>
+        <tr>
+          <Th>№</Th>
+          <Th>PRICE</Th>
+          <Th>AMOUNT</Th>
+          <Th>DATE</Th>
+        </tr>
+      </THead>
+
+      <tbody>
+        {items.map(({ id, price, amount, date }, idx) => {
+          return (
+            <Tr key={id}>
+              <Td>{idx + 1}</Td>
+              <Td>{price}</Td>
+              <Td>{amount}</Td>
+              <Td>{formatDate(date)}</Td>
+            </Tr>
+          );
+        })}
+      </tbody>
+    </BaseTable>
+  );
 };
